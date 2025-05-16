48e6980a36e1531c0bad8164f67c84959682567d
Akhila-reddy28 <kuntaakhila2004@gmail.com>
2025-05-16 13:52:37 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,20 @@
+const socket = io();
+const editor = document.getElementById('editor');
+const DOCUMENT_ID = "example-document-id";
+
+socket.emit('get-document', DOCUMENT_ID);
+
+socket.on('load-document', data => {
+  editor.value = data;
+});
+
+editor.addEventListener('input', () => {
+  const data = editor.value;
+  socket.emit('send-changes', data);
+  socket.emit('save-document', data);
+});
+
+socket.on('receive-changes', data => {
+  editor.value = data;
+});
+
